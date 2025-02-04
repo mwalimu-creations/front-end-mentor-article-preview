b725125973550117337c8627826dcf8bafac8b19
David Mwalimu <nzambulidavid@gmail.com>
2025-02-04 10:07:06 +0300

    Finished Solution
    Added the necessary JS to toggle the share menu and control its pop up as well as editing the README

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-const shareButton = document.getElementById('share-button')
-shareButton.addEventListener('click', (e) = {
-    
+const shareButton = document.getElementById('share-button');
+const shareMenu = document.querySelector('.share-menu');
+const largeScreen = window.matchMedia('(min-width:1024px)');
+
+function adjustMenu(e) {
+    shareMenu.style.bottom= e.matches ? '70px' : '0';
+}
+
+shareButton.addEventListener('click', () => {
+    adjustMenu(largeScreen);
 })
 
+document.addEventListener('click', (e) => {
+    if(!shareButton.contains(e.target) && !shareMenu.contains(e.target)) {
+        shareMenu.style.bottom='-1000px';
+    }
+})
+
+window.addEventListener('resize', () => {
+    shareMenu.style.bottom='-1000px';
+})
